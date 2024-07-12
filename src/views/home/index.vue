a42629ed0f7b09710ad823f2a0c5771e4008a112
MyFaith <854372534@qq.com>
2024-07-12 12:22:40 +0800

    首页

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,3 +1,12 @@
-<template>home</template>
+<template>
+  <h2>待开发</h2>
+</template>
 
 <script setup></script>
+
+<style lang="scss" scoped>
+h2 {
+  margin: 10% 0;
+  text-align: center;
+}
+</style>
