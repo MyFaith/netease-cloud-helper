8fe5fd8d6b0d00542c93c0f1a7c2756540145de7
MyFaith <854372534@qq.com>
2025-01-23 22:07:43 +0800

    优化代理配置并修正API目标路径

--- vite.config.js
+++ vite.config.js
@@ -6,15 +6,16 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 80
-  },
-  proxy: {
-    "/api": {
-      target: "http://localhost:3000",
-      changeOrigin: true,
-      rewrite: (path) => path.replace(/^\/api/, "")
+    port: 80,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000/",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, "")
+      }
     }
   },
+
   plugins: [vue()],
   resolve: {
     alias: {
