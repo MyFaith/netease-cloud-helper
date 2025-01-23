9879a9a02050f63db151d7d1443c37618937b615
MyFaith <854372534@qq.com>
2025-01-23 22:18:04 +0800

    优化Nginx配置，增加API代理及文件服务

--- vite.config.js
+++ vite.config.js
@@ -6,16 +6,8 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 80,
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000/",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, "")
-      }
-    }
+    port: 80
   },
-
   plugins: [vue()],
   resolve: {
     alias: {
