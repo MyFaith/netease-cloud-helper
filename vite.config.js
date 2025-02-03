f130fa6105d1bcda94bbd7f618393e95a1229ef1
MyFaith <854372534@qq.com>
2025-02-03 17:03:31 +0800

    本地跨域配置

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,14 @@ export default defineConfig({
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url))
     }
+  },
+  server: {
+    proxy: {
+      '/api/netease': {
+        target: 'http://localhost:3000', // 目标服务器地址
+        changeOrigin: true, // 允许跨域
+        rewrite: (path) => path.replace(/^\/api\/netease/, '') // 重写路径，去掉 /api/netease 前缀
+      }
+    }
   }
 });
